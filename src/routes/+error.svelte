196085bfe71da9dd4a675742e3922f743cbaa958
JK03Coder <jnkempton@gmail.com>
2023-10-14 17:48:48 +1300

    added default error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<div class="grid h-full px-4 place-content-center">
+  <div class="text-center">
+    <h1 class="font-black text-9xl">{$page.status}</h1>
+
+    <p class="text-2xl font-bold tracking-tight text-surface-900 sm:text-4xl">
+      {$page.error?.message}
+    </p>
+
+    <a href="/" class="btn btn-accent mt-6"> Go Back Home </a>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,71 +1,16 @@
-<article class="prose mx-auto mt-10">
-  <h1>Welcome to Roblox Pic</h1>
-
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Sodales ut eu sem
-    integer vitae justo eget magna. Euismod nisi porta lorem mollis aliquam ut
-    porttitor leo a. Rhoncus aenean vel elit scelerisque mauris. Integer enim
-    neque volutpat ac tincidunt vitae semper. Sapien nec sagittis aliquam
-    malesuada bibendum arcu vitae elementum curabitur. Ultrices vitae auctor eu
-    augue ut lectus. In egestas erat imperdiet sed euismod nisi. Tortor at risus
-    viverra adipiscing at. Bibendum enim facilisis gravida neque convallis a
-    cras semper. Non quam lacus suspendisse faucibus interdum posuere lorem.
-    Vulputate mi sit amet mauris commodo quis imperdiet massa. Velit aliquet
-    sagittis id consectetur purus. Auctor neque vitae tempus quam pellentesque
-    nec nam aliquam. Tellus id interdum velit laoreet. Pretium fusce id velit ut
-    tortor. Pharetra sit amet aliquam id diam maecenas. Lacus sed turpis
-    tincidunt id aliquet risus feugiat in ante. Vehicula ipsum a arcu cursus
-    vitae congue mauris. Mi quis hendrerit dolor magna eget est lorem.
-  </p>
-
-  <p>
-    Urna id volutpat lacus laoreet. Scelerisque purus semper eget duis at. Eget
-    dolor morbi non arcu risus. Ut venenatis tellus in metus. Amet aliquam id
-    diam maecenas ultricies. Ipsum dolor sit amet consectetur adipiscing elit
-    duis tristique sollicitudin. Mi eget mauris pharetra et. Porttitor leo a
-    diam sollicitudin tempor. Lectus proin nibh nisl condimentum id venenatis a
-    condimentum. Purus non enim praesent elementum facilisis leo vel fringilla.
-    Cursus risus at ultrices mi tempus.
-  </p>
-
-  <p>
-    Natoque penatibus et magnis dis parturient montes nascetur ridiculus mus.
-    Tempor nec feugiat nisl pretium fusce id velit ut. Laoreet id donec ultrices
-    tincidunt arcu non sodales. Sem nulla pharetra diam sit amet nisl suscipit
-    adipiscing bibendum. Viverra aliquet eget sit amet tellus cras. Scelerisque
-    viverra mauris in aliquam. Amet aliquam id diam maecenas ultricies mi eget
-    mauris pharetra. Malesuada fames ac turpis egestas. Tempus quam pellentesque
-    nec nam aliquam. Tortor at risus viverra adipiscing at. Eu augue ut lectus
-    arcu. Enim diam vulputate ut pharetra sit. Enim eu turpis egestas pretium
-    aenean pharetra. Pharetra vel turpis nunc eget lorem. Odio ut sem nulla
-    pharetra diam. Orci ac auctor augue mauris. Rhoncus dolor purus non enim.
-  </p>
-
-  <p>
-    Pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Tristique
-    senectus et netus et malesuada. Imperdiet proin fermentum leo vel orci porta
-    non. Urna porttitor rhoncus dolor purus non enim. Sit amet facilisis magna
-    etiam tempor orci eu lobortis elementum. Odio euismod lacinia at quis risus
-    sed vulputate odio. Posuere morbi leo urna molestie at elementum eu
-    facilisis sed. Libero nunc consequat interdum varius sit amet mattis.
-    Malesuada pellentesque elit eget gravida cum sociis natoque penatibus.
-    Volutpat blandit aliquam etiam erat velit scelerisque in. Augue ut lectus
-    arcu bibendum at varius vel pharetra vel. A cras semper auctor neque vitae
-    tempus quam pellentesque. Viverra mauris in aliquam sem fringilla ut morbi
-    tincidunt augue. Mattis vulputate enim nulla aliquet porttitor. Sit amet
-    porttitor eget dolor morbi non arcu. Viverra mauris in aliquam sem
-    fringilla.
-  </p>
-
-  <p>
-    Mattis rhoncus urna neque viverra justo nec. Maecenas volutpat blandit
-    aliquam etiam erat velit scelerisque in. Lectus mauris ultrices eros in
-    cursus turpis massa tincidunt. Massa sapien faucibus et molestie ac feugiat.
-    Nisl suscipit adipiscing bibendum est ultricies. Vitae tempus quam
-    pellentesque nec nam aliquam sem. Viverra ipsum nunc aliquet bibendum. Velit
-    egestas dui id ornare arcu odio ut sem. Mattis molestie a iaculis at erat
-    pellentesque adipiscing commodo elit. Posuere morbi leo urna molestie at
-    elementum.
-  </p>
-</article>
+<div class="hero min-h-full">
+  <div class="hero-content flex-col lg:flex-row">
+    <div>
+      <h1 class="text-6xl font-bold uppercase">Generate a stunning gfx</h1>
+      <p class="py-6">
+        This tool allows you to create beautiful renders of Roblox characters in
+        any scenario for profile pictures, banners, or game icons.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+    <img
+      src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+      class="max-w-sm rounded-lg shadow-2xl"
+    />
+  </div>
+</div>
