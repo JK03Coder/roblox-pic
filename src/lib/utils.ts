9196180dae98f7a5de876bf735256c253e8c35ea
JK03Coder <jnkempton@gmail.com>
2023-10-22 23:22:59 +1300

    viewTransitionAPI utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+import { tick } from "svelte";
+
+// don't forget to add the view-transition-name css property to the transitioning elements
+export function executeViewTransition(callback: Function) {
+  // @ts-ignore
+  if (!document.startViewTransition) {
+    callback();
+  }
+
+  // @ts-ignore
+  return document.startViewTransition(async () => {
+    callback();
+    await tick();
+  });
+}
