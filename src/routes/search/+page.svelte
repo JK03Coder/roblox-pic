c65d90d459fb036f2b96c1fd1118827bed821716
JK03Coder <jnkempton@gmail.com>
2024-02-06 15:04:55 +1300

    ad

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -183,4 +183,22 @@
       </button>
     </div>
   </div>
+
+  <script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6027683009484183"
+    crossorigin="anonymous"
+  ></script>
+  <!-- Search page -->
+  <ins
+    class="adsbygoogle"
+    style="display:block"
+    data-ad-client="ca-pub-6027683009484183"
+    data-ad-slot="1264564296"
+    data-ad-format="auto"
+    data-full-width-responsive="true"
+  ></ins>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
 </div>
